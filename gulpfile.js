69f5f4aa0bf2f68a3a09ac9008b0e32790979940
Ray Villalobos <ray@planetoftheweb.com>
2020-09-18 19:48:47 -0400

    New Branch Order, update to gulp 4 and other modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,60 @@
-const gulp = require('gulp'),
-  webserver = require('gulp-webserver'),
-  babel = require('gulp-babel'),
-  gutil = require('gulp-util'),
-  sourcemaps = require('gulp-sourcemaps'),
+let gulp = require('gulp'),
   sass = require('gulp-sass'),
+  babel = require('gulp-babel'),
+  webserver = require('gulp-server-io'),
   postcss = require('gulp-postcss'),
-  cssnext = require('postcss-cssnext'),
+  autoprefixer = require('autoprefixer'),
+  source = './process',
+  dest = './builds/masteringcode'
 
-  browserSupport = ['last 2 versions', 'ie 10'],
+function html() {
+  return gulp.src(dest + '**/*.html')
+}
 
-  source = 'process/',
-  dest = 'builds/masteringcode/';
+function js() {
+  return gulp
+    .src(source + '/js/script.js')
+    .pipe(
+      babel({
+        presets: ['@babel/preset-env'],
+      })
+    )
+    .pipe(gulp.dest(dest + '/js'))
+}
 
-gulp.task('html', function() {
-  gulp.src(dest + '**/*.html');
-});
+function processSass() {
+  return gulp
+    .src(source + '/scss/style.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest(dest + '/css'))
+}
 
-// Regular CSS
-gulp.task('css', function() {
-  gulp.src(dest + '**/*.css');
-});
+function styles() {
+  return gulp
+    .src(source + '/postcss/style.css')
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gulp.dest(dest + '/css'))
+}
 
-// Post CSS
-gulp.task('postcss', function() {
-  gulp.src(source + 'postcss/style.css')
-  .pipe(sourcemaps.init())
-  .pipe(postcss([
-    cssnext({ browsers: browserSupport })
-  ]))
-  .on('error', gutil.log)
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest(dest + 'css'));
-});
+function watch() {
+  gulp.watch(source + '/js/**/*.js', js)
+  gulp.watch(source + '/postcss/**/*.css', styles)
+  gulp.watch(source + '/scss/style.scss', processSass)
+  gulp.watch(source + '/index.html', html)
+}
 
-// Sass
-gulp.task('sass', function() {
-  gulp.src(source + 'scss/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(dest + 'css'));
-});
+function server() {
+  return gulp.src(dest).pipe(
+    webserver({
+      serverReload: {
+        dir: dest,
+      },
+      open: true,
+    })
+  )
+}
 
-// JavaScript ES6
-gulp.task('js', function() {
-  gulp.src(source + 'js/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .on('error', gutil.log)
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(dest + 'js'));
-});
+var build = gulp.series(gulp.parallel(js, styles, processSass, html), server, watch)
 
-gulp.task('watch', function() {
-  gulp.watch(source + '**/*.js', ['js']);
-  gulp.watch(dest + '**/*.css', ['css']); //CSS
-  gulp.watch(source + 'postcss/**/*.css', ['postcss']); // PostCSS
-  gulp.watch(source + 'scss/**/*.scss', ['sass']); // sass
-  gulp.watch(dest + '**/*.html', ['html']);
-});
-
-gulp.task('webserver', function() {
-  gulp.src(dest)
-    .pipe(webserver({
-      livereload: true,
-      port: 3000,
-      open: true
-    }));
-  gulp.src('docs')
-    .pipe(webserver({
-      port: 3333,
-    }));
-    
-});
-
-gulp.task('default', ['html', 'postcss', 'js', 'webserver','watch']);
+gulp.task('default', build)
