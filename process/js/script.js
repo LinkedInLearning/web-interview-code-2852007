6e07fd8bd66db10ac0bfa2c76fd04a3ffd5b6e0c
Ray Villalobos <ray@planetoftheweb.com>
2020-09-18 19:26:06 -0400

    updated course to use new version of gulp and associated plugins

--- process/js/script.js
+++ process/js/script.js
@@ -1,32 +1,36 @@
-var artists, calendar;
+var artists, calendar
 
 function loadData(url) {
   return new Promise(function (resolve, reject) {
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
+    var req = new XMLHttpRequest()
+    req.open('GET', url)
     req.onload = function () {
       if (req.status == 200) {
-        console.log('getting ' + url);
+        console.log('getting ' + url)
         resolve(JSON.parse(req.response))
       } else {
         reject(Error('Problem loading data'))
       }
     } // onload callback
-    req.send();
-  });
+    req.send()
+  })
 }
 
-calendar = loadData('js/data_schedule.json');
-artists = loadData('js/data_artists.json');
+calendar = loadData('js/data_schedule.json')
+artists = loadData('js/data_artists.json')
 
-document.querySelector('.load-info')
-  .addEventListener('click', function () {
-    calendar.then(function (data) {
-      console.log(data);
-      return artists;
-    }, function (error) {
-      console.log('Problem', error);
-    }).then(function (otherData) {
-      console.log(otherData);
-    });
-  });
+document.querySelector('.load-info').addEventListener('click', function () {
+  calendar
+    .then(
+      function (data) {
+        console.log(data)
+        return artists
+      },
+      function (error) {
+        console.log('Problem', error)
+      }
+    )
+    .then(function (otherData) {
+      console.log(otherData)
+    })
+})
